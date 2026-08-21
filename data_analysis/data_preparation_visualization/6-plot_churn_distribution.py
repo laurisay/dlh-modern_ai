efea6f179a63efd13cc8344f9d8e66f0b707a113
laurisay <assamsayah5@gmail.com>
2026-08-22 01:37:03 +0200

    6-plot_churn_distribution.py2

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -11,10 +11,9 @@ def plot_churn_distribution(df):
 
     counts = df['Churn'].value_counts()
 
-    plt.bar(
-        counts.index,
-        counts.values,
-        color=['skyblue', 'salmon']
-    )
+    colors = ['skyblue' if value == 'No' else 'salmon'
+              for value in counts.index]
+
+    plt.bar(counts.index, counts.values, color=colors)
 
     plt.show()
