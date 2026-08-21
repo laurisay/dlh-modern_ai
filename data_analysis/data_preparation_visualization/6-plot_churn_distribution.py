87e46b12e35e5fceca3aee1bdabc5766d8b5fe44
laurisay <assamsayah5@gmail.com>
2026-08-22 01:35:00 +0200

    6-plot_churn_distribution.py2

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-"""Plot the distribution of churn."""
-
+"""
+"""
 import matplotlib.pyplot as plt
 
 
 def plot_churn_distribution(df):
-    """Plot the distribution of the Churn column."""
+    """
+    """
+    plt.figure(figsize=(12, 8))
+
     counts = df['Churn'].value_counts()
-    plt.bar(counts.index, counts.values,
-            color=['skyblue', 'salmon'])
+
+    plt.bar(
+        counts.index,
+        counts.values,
+        color=['skyblue', 'salmon']
+    )
+
     plt.show()
