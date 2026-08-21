6d7e9c05726767c045f3920ee2ebfc61ff43661f
laurisay <assamsayah5@gmail.com>
2026-08-22 01:51:31 +0200

    6-plot_churn_distribution.py7

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -10,14 +10,7 @@ def plot_churn_distribution(df):
     Visualizes churn class distribution as a bar plot.
     """
     plt.figure(figsize=(12, 8))
-
     counts = df['Churn'].value_counts()
-
-    plt.bar(
-        counts.index,
-        counts.values,
-        color=['skyblue' if x == 'No' else 'salmon'
-               for x in counts.index]
-    )
-
+    plt.bar(counts.index, counts.values,
+            color=['skyblue', 'salmon'])
     plt.show()
