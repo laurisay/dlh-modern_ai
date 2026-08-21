2df9d7726b91992b00077484b66121782445bf7b
laurisay <assamsayah5@gmail.com>
2026-08-22 00:47:56 +0200

    test18

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -8,13 +8,13 @@ import numpy as np
 
 def plot_missingness(df):
     """
-    Plot missing values in a DataFrame.
+    Plot the missing values in a DataFrame.
     """
     plt.figure(figsize=(12, 8))
 
     missing = np.where(df.isnull())
 
-    plt.plot(missing[0], missing[1], '|')
+    plt.scatter(missing[0], missing[1], marker='|')
 
     plt.yticks(np.arange(len(df.columns)), df.columns)
     plt.title("Missingness Plot")
